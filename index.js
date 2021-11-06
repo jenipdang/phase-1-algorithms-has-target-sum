7c35b2fd83cf2c1f43a85bf2cba356a82e2469e3
Jeni Dang <jenipdang@yahoo.com>
2021-11-06 09:16:05 -0700

    commit tree 70074a725d6a1cf948db80ebd3eb03c9c9f12b40

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
+// const hasTargetSum = (array, target) => {
+//   let storageHash = {}
+//   let nums = []
+  
+//   for(let i in array){
+//     let addend = target - array[i]
+      
+//       if (addend in storageHash){
+//           nums.push([addend, array[i]])
+//       }
+//       storageHash[array[i]] = i
+//   }
+//   return nums
+// }
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  //iterate through each number in the array
+  for (let i = 0; i < array.length; i++){
+    // for the current num, identify a complement that adds to the targt (comp = target - num)
+    const complement = target - array[i]
+    // iterate through the rest of the array
+    for (let j = i + 1; j < array.length; j++) {
+      // check if any number is our complment
+      // if so, return true
+      if (array[j] === complement) return true
+    }
+  }
+  // if I reach the end of the array, return false
+  return false
 }
 
 /* 
@@ -8,10 +35,21 @@ function hasTargetSum(array, target) {
 
 /* 
   Add your pseudocode here
+  iterate through each number in the array
+    for the current num, identify a complement that adds to the target (comp = target - num)
+    iterate through the rest of the array
+      check if any number is our complement
+      if so, return true
+    
+  if I reach the end of the array, return false
+ 
 */
 
 /*
   Add written explanation of your solution here
+   make a function `hasTargetSum` that checks if two numbers from the array add up to some target. 
+  Example, if I have an array [1,2,3,4] and the target is 6 I should return true because 2 and 4 to 6.
+  for each number check if there's a number that adds to the target
 */
 
 // You can run `node index.js` to view these console logs
